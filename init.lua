2a779ebf86fe6a617d3dcd41ef2bbd67b43184e8
Steve Laing <steve.laing@gmail.com>
2023-03-29 14:52:24 +0100

    Tweaks to get nv-ide working locally

---
+++ init.lua
@@ -0,0 +1,14 @@
+require('config.settings')
+require('config.color')
+-- Lazy
+require('config.lazy')
+--
+require('util.hjkl_notifier')
+require('util.new_note')
+-- Select theme.lua fix feline and nvim-cokeline
+require('config.colorschemes.gruvbox')
+--
+require('config.font')
+require('config.completion')
+-- Keymap
+require('config.mappings')
